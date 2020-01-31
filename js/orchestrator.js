40bf8d1d5f458f7511d052e599003d628fbd9884
Leonardo Sarra <leonardosarra@outlook.com>
2020-01-31 11:45:12 +0100

    Add orchestrator for data

---
+++ js/orchestrator.js
@@ -0,0 +1,46 @@
+Orchestrator = function () {
+    this.dataLoaded = false;
+    this.data = []
+    this.highlightBrush = []
+    this.highlighOthers = []
+    this.listenersContainer = new EventTarget();
+}
+Orchestrator.prototype.loadData = function () {
+    _obj = this;
+    d3.csv("./data/out_modis_20200129.csv", function (loadedData) {
+        for (i = 0; i < loadedData.length; i++) {
+            day = new Date(loadedData[i].acq_date).getDay();
+            if (day == 0) dayOfWeek = 'Monday';
+            else if (day == 1) dayOfWeek = 'Tuesday';
+            else if (day == 2) dayOfWeek = 'Wednesday';
+            else if (day == 3) dayOfWeek = 'Thursday';
+            else if (day == 4) dayOfWeek = 'Friday';
+            else if (day == 5) dayOfWeek = 'Saturday';
+            else dayOfWeek = 'Sunday';
+            loadedData[i].dayOfWeek = dayOfWeek;
+            _obj.data.push(loadedData[i])
+        }
+        _obj.dataLoaded = true;
+        _obj.listenersContainer.dispatchEvent(new Event('dataReady'))
+    })
+}
+
+Orchestrator.prototype.addListener = function (nameEvent, functionz) {
+    if (this.dataLoaded && nameEvent == 'dataReady') functionz();
+    else this.listenersContainer.addEventListener(nameEvent, functionz);
+}
+
+Orchestrator.prototype.notifyBrushHighlight = function () {
+    this.listenersContainer.dispatch(new Event('brushHightlight'));
+}
+
+Orchestrator.prototype.notifyOtherHighlight = function () {
+    this.listenersContainer.dispatch(new Event('otherHighlight'));
+}
+
+Orchestrator.prototype.notifyDataChanged = function () {
+    this.listenersContainer.dispatch(new Event('dataChanged'));
+}
+
+var orchestrator = new Orchestrator();
+orchestrator.loadData();
