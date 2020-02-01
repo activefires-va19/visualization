4f2674fe12988a9c1c2ea8733c4846fd5223f6b2
AlexS1995 <sarpe.1692247@studenti.uniroma1.it>
2020-02-01 12:14:12 +0100

    Add support to change desired histogram

--- js/Histogram_chart.js
+++ js/Histogram_chart.js
@@ -13,6 +13,7 @@ var parseHour = d3.timeParse("%H%M");
 var parseDays = d3.timeParse("%Y-%m-%d-%H%M");
 var events_hour = [];
 var events_days = [];
+var x_h, y_h;
 
 function parse_hour_h(input){
   h = input.substring(0, 2);
@@ -28,11 +29,46 @@ function parse_hour_h(input){
 orchestrator.addListener('dataReady', function (e) {
   data = orchestrator.data;
   data.forEach( e => {
-  events_hour.push(parse_hour_h(e["acq_time"]));
-  events_days.push(parseDays(e["acq_date"]+"-"+e["acq_time"]));
+    events_hour.push(parse_hour_h(e["acq_time"]));
+    events_days.push(parseDays(e["acq_date"]+"-"+e["acq_time"]));
   });
-  update_histogram(events_hour);
-  //update_histogram(events_hour);
+
+  low_h = parse_hour_h("0000");
+  high_h = parse_hour_h("2359");
+  ticks = 24;
+
+  x_h = d3.scaleTime()
+      .domain([low_h, high_h])
+      .range([0, width_h]);
+
+  svg_h.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(0," + height_h + ")")
+      .call(d3.axisBottom(x_h));
+
+  var histogram = d3.histogram()
+      .value(function(d) { return d; })
+      .domain(x_h.domain())
+      .thresholds(x_h.ticks(ticks));
+
+  var bins = histogram(events_hour);
+ 
+  y_h = d3.scaleLinear().range([height_h, 0]);
+
+  y_h.domain([0, d3.max(bins, function(d) { return d.length; })]);
+
+  svg_h.append("g").attr("class", "y-axis").call(d3.axisLeft(y_h));
+
+  svg_h.selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+        .attr("class", "histo_bars")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x_h(d.x0) + "," + y_h(d.length) + ")"; })
+        .attr("width", function(d) { return x_h(d.x1) - x_h(d.x0)-1 ; })
+        .attr("height", function(d) { return height_h - y_h(d.length); })
+        .style("fill", "#6fb1f3")
 });
 
 
@@ -49,13 +85,7 @@ function update_histogram(data_selected){
   }
   
 
-  var x_h = d3.scaleTime()
-      .domain([low_h, high_h])
-      .range([0, width_h]);
-
-  svg_h.append("g")
-      .attr("transform", "translate(0," + height_h + ")")
-      .call(d3.axisBottom(x_h));
+  x_h.domain([low_h, high_h]);
 
   var histogram = d3.histogram()
       .value(function(d) { return d; })
@@ -63,20 +93,29 @@ function update_histogram(data_selected){
       .thresholds(x_h.ticks(ticks));
 
   var bins = histogram(data_selected);
- 
-  var y_h = d3.scaleLinear().range([height_h, 0]);
 
   y_h.domain([0, d3.max(bins, function(d) { return d.length; })]);
 
-  svg_h.append("g").call(d3.axisLeft(y_h));
+  svg_h.select(".x-axis").transition().duration(1).call(d3.axisBottom(x_h));
 
-  svg_h.selectAll("rect")
-      .data(bins)
-      .enter()
+  svg_h.select(".y-axis").transition().duration(1).call(d3.axisLeft(y_h));
+
+  svg_h.selectAll(".histo_bars").remove();
+
+  svg_h.selectAll("rect").data(bins).enter()
       .append("rect")
         .attr("x", 1)
+        .attr("class", "histo_bars")
         .attr("transform", function(d) { return "translate(" + x_h(d.x0) + "," + y_h(d.length) + ")"; })
         .attr("width", function(d) { return x_h(d.x1) - x_h(d.x0)-1 ; })
         .attr("height", function(d) { return height_h - y_h(d.length); })
         .style("fill", "#6fb1f3")
 }
+
+d3.select("#hours").on("click", function(){
+  update_histogram(events_hour);
+});
+
+d3.select("#week").on("click", function(){
+  update_histogram(events_days);
+});
