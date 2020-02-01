e1ff07550c6627c265eaca55fb4d4944a44f37ce
Leonardo Sarra <leonardosarra@outlook.com>
2020-02-01 13:27:42 +0100

    Make frp boxplot subject to filtering

--- js/Boxplot_Frp_chart.js
+++ js/Boxplot_Frp_chart.js
@@ -6,7 +6,7 @@ var svg_box_b3 = d3.select(".box3")
     .attr("transform", "translate(" + margin_box_b.left + "," + margin_box_b.top + ")");
 
 orchestrator.addListener('dataReady', function (e) {
-    data = orchestrator.data;
+    data = evalData();
     var points_frp = [];
     data.forEach(elem => {
         points_frp.push(+elem["frp"]);
@@ -21,17 +21,17 @@ orchestrator.addListener('dataReady', function (e) {
     var min3 = first_quantile3 - 1.5 * interQuantileRange3;
     var max3 = first_quantile3 + 1.5 * interQuantileRange3;
 
-    if(min3 < 0) min3 = 0;
+    if (min3 < 0) min3 = 0;
 
     var center3 = 50;
     var width3 = 25;
 
     var low3;
 
-    if(min3 - 10 < 0){
+    if (min3 - 10 < 0) {
         low3 = 0;
     }
-    else{
+    else {
         low3 = (min3 - 10);
     }
 
@@ -43,9 +43,10 @@ orchestrator.addListener('dataReady', function (e) {
 
     var yAxis3 = d3.axisLeft(y3);
 
-    svg_box_b3.append("g").call(yAxis3.ticks(8));
+    svg_box_b3.append("g").attr('class', "y_axis").call(yAxis3.ticks(8));
 
     svg_box_b3.append("line")
+        .attr('class', "box_line_vertical")
         .attr("x1", center3)
         .attr("x2", center3)
         .attr("y1", y3(min3))
@@ -53,6 +54,7 @@ orchestrator.addListener('dataReady', function (e) {
         .attr("stroke", "black");
 
     svg_box_b3.append("rect")
+        .attr('class', "box_rect")
         .attr("x", center3 - width3 / 2)
         .attr("y", y3(third_quantile3))
         .attr("height", (y3(first_quantile3) - y3(third_quantile3)))
@@ -64,6 +66,7 @@ orchestrator.addListener('dataReady', function (e) {
         .data([min3, median3, max3])
         .enter()
         .append("line")
+        .attr('class', "box_line_horizontal")
         .attr("x1", center3 - width3 / 2)
         .attr("x2", center3 + width3 / 2)
         .attr("y1", function (d) { return (y3(d)) })
@@ -78,4 +81,84 @@ orchestrator.addListener('dataReady', function (e) {
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("Frp");
+
+    orchestrator.addListener('parallelBrushing', function (e) {
+        update();
+    });
+
+    function update() {
+        data = evalData();
+        var points_frp = [];
+        data.forEach(elem => {
+            points_frp.push(+elem["frp"]);
+        });
+
+        data_len = points_frp.length;
+        if (data_len == 0) {
+            svg_box_b2.select(".y_axis").style('display', "none");
+            svg_box_b2.select('.box_line_vertical').style('display', "none");
+            svg_box_b2.select('.box_rect').style('display', "none");
+            svg_box_b2.selectAll('.box_line_horizontal').style('display', "none");
+            return;
+        }
+
+        //Calculating median and quantiles for boxplot
+        var data_sorted3 = points_frp.sort(d3.ascending);
+        var first_quantile3 = d3.quantile(data_sorted3, .25);
+        var median3 = d3.quantile(data_sorted3, .5);
+        var third_quantile3 = d3.quantile(data_sorted3, .75);
+        var interQuantileRange3 = third_quantile3 - first_quantile3;
+        var min3 = first_quantile3 - 1.5 * interQuantileRange3;
+        var max3 = first_quantile3 + 1.5 * interQuantileRange3;
+
+        if (min3 < 0) min3 = 0;
+
+        var center3 = 50;
+        var width3 = 25;
+
+        var low3;
+
+        if (min3 - 10 < 0) {
+            low3 = 0;
+        }
+        else {
+            low3 = (min3 - 10);
+        }
+
+        var high3 = (max3 + 10);
+
+        var y3 = d3.scaleLinear()
+            .domain([low3, high3])
+            .range([height_box_b, 0]);
+
+        var yAxis3 = d3.axisLeft(y3);
+
+        svg_box_b3.select(".y_axis").style("display", null).call(yAxis3.ticks(8));
+
+        svg_box_b3.select('.box_line_vertical')
+            .attr("x1", center3)
+            .attr("x2", center3)
+            .attr("y1", y3(min3))
+            .attr("y2", y3(max3))
+            .attr("stroke", "black");
+
+        svg_box_b3.select('.box_rect')
+            .attr("x", center3 - width3 / 2)
+            .attr("y", y3(third_quantile3))
+            .attr("height", (y3(first_quantile3) - y3(third_quantile3)))
+            .attr("width", width3)
+            .attr("stroke", "black")
+            .style("fill", "#6fb1f3");
+
+        svg_box_b3.selectAll('.box_line_horizontal')
+            .data([min3, median3, max3])
+            .attr("x1", center3 - width3 / 2)
+            .attr("x2", center3 + width3 / 2)
+            .attr("y1", function (d) { return (y3(d)) })
+            .attr("y2", function (d) { return (y3(d)) })
+            .attr("stroke", "black");
+    }
+    function evalData() {
+        return orchestrator.getDataFilteredByParallel();
+    }
 })
