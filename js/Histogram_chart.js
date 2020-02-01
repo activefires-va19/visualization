3fab465767df41b99c9cd6a843f8e8dad7bbdc5e
AlexS1995 <sarpe.1692247@studenti.uniroma1.it>
2020-02-01 14:34:11 +0100

    Make histograms subjected to filtering

--- js/Histogram_chart.js
+++ js/Histogram_chart.js
@@ -14,6 +14,7 @@ var parseDays = d3.timeParse("%Y-%m-%d-%H%M");
 var events_hour = [];
 var events_days = [];
 var x_h, y_h;
+var selected_h = 1;
 
 function parse_hour_h(input){
   h = input.substring(0, 2);
@@ -27,7 +28,7 @@ function parse_hour_h(input){
 }
 
 orchestrator.addListener('dataReady', function (e) {
-  data = orchestrator.data;
+  data = evalData();
   data.forEach( e => {
     events_hour.push(parse_hour_h(e["acq_time"]));
     events_days.push(parseDays(e["acq_date"]+"-"+e["acq_time"]));
@@ -70,74 +71,93 @@ orchestrator.addListener('dataReady', function (e) {
         .attr("width", function(d) { return x_h(d.x1) - x_h(d.x0)-1 ; })
         .attr("height", function(d) { return height_h - y_h(d.length); })
         .style("fill", "#6fb1f3")
-});
 
+  function update_histogram(num){
 
-function update_histogram(data_selected){
-  if(data_selected == events_hour){
-    low_h = parse_hour_h("0000");
-    high_h = parse_hour_h("2359");
-    ticks = 24;
-  }
-  else{
-    day_l = events_days[0].getDate();
-    month_l = events_days[0].getMonth()+1;
-    year_l = events_days[0].getFullYear();
-    if(month_l == 13){
-      month_l = 1;
-      year_l +=1;
+    selected_h = num;
+    data = evalData();
+
+    events_days = [];
+    events_hour = [];
+
+    data.forEach( e => {
+      events_hour.push(parse_hour_h(e["acq_time"]));
+      events_days.push(parseDays(e["acq_date"]+"-"+e["acq_time"]));
+    });
+    events_days.sort(function(a,b){return a.getTime() - b.getTime()});
+
+    if(selected_h == 1){
+      low_h = parse_hour_h("0000");
+      high_h = parse_hour_h("2359");
+      ticks = 24;
+      selected_set = events_hour;
     }
-    string_l = String(year_l)+"-"+String(month_l)+"-"+String(day_l)+"-"+"0000";
+    else{
+      day_l = events_days[0].getDate();
+      month_l = events_days[0].getMonth()+1;
+      year_l = events_days[0].getFullYear();
+      if(month_l == 13){
+        month_l = 1;
+        year_l +=1;
+      }
+      string_l = String(year_l)+"-"+String(month_l)+"-"+String(day_l)+"-"+"0000";
 
-    day_h = events_days[events_days.length-1].getDate();
-    month_h = events_days[events_days.length-1].getMonth()+1;
-    year_h = events_days[events_days.length-1].getFullYear();
-    if(month_h == 13){
-      month_h = 1;
-      year_h +=1;
+      day_h = events_days[events_days.length-1].getDate();
+      month_h = events_days[events_days.length-1].getMonth()+1;
+      year_h = events_days[events_days.length-1].getFullYear();
+      if(month_h == 13){
+        month_h = 1;
+        year_h +=1;
+      }
+      string_h = String(year_h)+"-"+String(month_h)+"-"+String(day_h)+"-"+"2359";
+
+      low_h = parseDays(string_l);
+      high_h = parseDays(string_h);
+      ticks = 24;
+      selected_set = events_days;
     }
-    string_h = String(year_h)+"-"+String(month_h)+"-"+String(day_h)+"-"+"2359";
+    
 
-    low_h = parseDays(string_l);
-    high_h = parseDays(string_h);
-    ticks = 24;
+    x_h.domain([low_h, high_h]);
+
+    var histogram = d3.histogram()
+        .value(function(d) { return d; })
+        .domain(x_h.domain())
+        .thresholds(x_h.ticks(ticks));
+
+    var bins = histogram(selected_set);
+
+    y_h.domain([0, d3.max(bins, function(d) { return d.length; })]);
+
+    svg_h.select(".x-axis").transition().duration(1).call(d3.axisBottom(x_h));
+
+    svg_h.select(".y-axis").transition().duration(1).call(d3.axisLeft(y_h));
+
+    svg_h.selectAll(".histo_bars").remove();
+
+    svg_h.selectAll("rect").data(bins).enter()
+        .append("rect")
+          .attr("x", 1)
+          .attr("class", "histo_bars")
+          .attr("transform", function(d) { return "translate(" + x_h(d.x0) + "," + y_h(d.length) + ")"; })
+          .attr("width", function(d) { return x_h(d.x1) - x_h(d.x0)-1 ; })
+          .attr("height", function(d) { return height_h - y_h(d.length); })
+          .style("fill", "#6fb1f3")
   }
-  
 
-  x_h.domain([low_h, high_h]);
+  orchestrator.addListener('parallelBrushing', function (e) {
+    update_histogram(selected_h);
+  });
 
-  var histogram = d3.histogram()
-      .value(function(d) { return d; })
-      .domain(x_h.domain())
-      .thresholds(x_h.ticks(ticks));
+  d3.select("#hours").on("click", function(){
+    update_histogram(1);
+  });
 
-  console.log(data_selected);
+  d3.select("#week").on("click", function(){
+    update_histogram(2);
+  });
 
-  var bins = histogram(data_selected);
-  console.log(bins);
-
-  y_h.domain([0, d3.max(bins, function(d) { return d.length; })]);
-
-  svg_h.select(".x-axis").transition().duration(1).call(d3.axisBottom(x_h));
-
-  svg_h.select(".y-axis").transition().duration(1).call(d3.axisLeft(y_h));
-
-  svg_h.selectAll(".histo_bars").remove();
-
-  svg_h.selectAll("rect").data(bins).enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("class", "histo_bars")
-        .attr("transform", function(d) { return "translate(" + x_h(d.x0) + "," + y_h(d.length) + ")"; })
-        .attr("width", function(d) { return x_h(d.x1) - x_h(d.x0)-1 ; })
-        .attr("height", function(d) { return height_h - y_h(d.length); })
-        .style("fill", "#6fb1f3")
-}
-
-d3.select("#hours").on("click", function(){
-  update_histogram(events_hour);
-});
-
-d3.select("#week").on("click", function(){
-  update_histogram(events_days);
+  function evalData() {
+    return orchestrator.getDataFilteredByParallel();
+  }
 });
