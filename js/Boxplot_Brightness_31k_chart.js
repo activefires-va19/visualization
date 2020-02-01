a2111a06384485287b0a0e07fd0b6081acbd0dfd
Leonardo Sarra <leonardosarra@outlook.com>
2020-02-01 13:27:22 +0100

    Make brightness31 boxplot subject to filtering

--- js/Boxplot_Brightness_31k_chart.js
+++ js/Boxplot_Brightness_31k_chart.js
@@ -6,7 +6,7 @@ var svg_box_b2 = d3.select(".box2")
     .attr("transform", "translate(" + margin_box_b.left + "," + margin_box_b.top + ")");
 
 orchestrator.addListener('dataReady', function (e) {
-    data = orchestrator.data;
+    data = evalData();
     var points_brightness_31k = [];
     data.forEach(elem => {
         points_brightness_31k.push(+elem["bright_t31"]);
@@ -20,17 +20,17 @@ orchestrator.addListener('dataReady', function (e) {
     var min2 = first_quantile2 - 1.5 * interQuantileRange2;
     var max2 = first_quantile2 + 1.5 * interQuantileRange2;
 
-    if(min2 < 0) min2 = 0;
+    if (min2 < 0) min2 = 0;
 
     var center2 = 50;
     var width2 = 25;
 
     var low2;
 
-    if(min2 - 10 < 0){
+    if (min2 - 10 < 0) {
         low2 = 0;
     }
-    else{
+    else {
         low2 = (min2 - 10);
     }
 
@@ -42,9 +42,10 @@ orchestrator.addListener('dataReady', function (e) {
 
     var yAxis2 = d3.axisLeft(y2);
 
-    svg_box_b2.append("g").call(yAxis2.ticks(8));
+    svg_box_b2.append("g").attr('class', "y_axis").call(yAxis2.ticks(6));
 
     svg_box_b2.append("line")
+        .attr('class', "box_line_vertical")
         .attr("x1", center2)
         .attr("x2", center2)
         .attr("y1", y2(min2))
@@ -52,6 +53,7 @@ orchestrator.addListener('dataReady', function (e) {
         .attr("stroke", "black");
 
     svg_box_b2.append("rect")
+        .attr('class', "box_rect")
         .attr("x", center2 - width2 / 2)
         .attr("y", y2(third_quantile2))
         .attr("height", (y2(first_quantile2) - y2(third_quantile2)))
@@ -59,10 +61,11 @@ orchestrator.addListener('dataReady', function (e) {
         .attr("stroke", "black")
         .style("fill", "#6fb1f3");
 
-    svg_box_b2.selectAll("wings")
+    svg_box_b2.selectAll("wings").attr('class', "box_wings")
         .data([min2, median2, max2])
         .enter()
         .append("line")
+        .attr('class', "box_line_horizontal")
         .attr("x1", center2 - width2 / 2)
         .attr("x2", center2 + width2 / 2)
         .attr("y1", function (d) { return (y2(d)) })
@@ -77,4 +80,85 @@ orchestrator.addListener('dataReady', function (e) {
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("Brightness 31k");
-})
+
+    orchestrator.addListener('parallelBrushing', function (e) {
+        update();
+    });
+
+    function update() {
+        data = evalData();
+        var points_brightness_31k = [];
+        data.forEach(elem => {
+            points_brightness_31k.push(+elem["bright_t31"]);
+        });
+        var data_len = points_brightness_31k.length;
+        if (data_len == 0) {
+            svg_box_b2.select(".y_axis").style('display', "none");
+            svg_box_b2.select('.box_line_vertical').style('display', "none");
+            svg_box_b2.select('.box_rect').style('display', "none");
+            svg_box_b2.selectAll('.box_line_horizontal').style('display', "none");
+            return;
+        }
+        var data_sorted2 = points_brightness_31k.sort(d3.ascending);
+        var first_quantile2 = d3.quantile(data_sorted2, .25);
+        var median2 = d3.quantile(data_sorted2, .5);
+        var third_quantile2 = d3.quantile(data_sorted2, .75);
+        var interQuantileRange2 = third_quantile2 - first_quantile2;
+        var min2 = first_quantile2 - 1.5 * interQuantileRange2;
+        var max2 = first_quantile2 + 1.5 * interQuantileRange2;
+    
+        if (min2 < 0) min2 = 0;
+    
+        var center2 = 50;
+        var width2 = 25;
+    
+        var low2;
+    
+        if (min2 - 10 < 0) {
+            low2 = 0;
+        }
+        else {
+            low2 = (min2 - 10);
+        }
+    
+        var high2 = (max2 + 10);
+    
+        var y2 = d3.scaleLinear()
+            .domain([low2, high2])
+            .range([height_box_b, 0]);
+    
+        var yAxis2 = d3.axisLeft(y2);
+    
+        svg_box_b2.select(".y_axis").style("display",null).call(yAxis2.ticks(6));
+    
+        svg_box_b2.select('.box_line_vertical')
+            .attr("x1", center2)
+            .attr("x2", center2)
+            .attr("y1", y2(min2))
+            .attr("y2", y2(max2))
+            .attr("stroke", "black")
+            .style("display",null);
+    
+        svg_box_b2.select('.box_rect')
+            .attr("x", center2 - width2 / 2)
+            .attr("y", y2(third_quantile2))
+            .attr("height", (y2(first_quantile2) - y2(third_quantile2)))
+            .attr("width", width2)
+            .attr("stroke", "black")
+            .style("fill", "#6fb1f3")
+            .style("display",null);
+    
+        svg_box_b2.selectAll('.box_line_horizontal')
+            .data([min2, median2, max2])    
+            .attr("x1", center2 - width2 / 2)
+            .attr("x2", center2 + width2 / 2)
+            .attr("y1", function (d) { return (y2(d)) })
+            .attr("y2", function (d) { return (y2(d)) })
+            .attr("stroke", "black")
+            .style("display",null);
+    }
+    function evalData() {
+        return orchestrator.getDataFilteredByParallel();
+    }
+});
+
