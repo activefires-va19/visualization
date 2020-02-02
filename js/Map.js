8133317cb59da4eedc5771bd59b730a525ef6770
Leonardo Sarra <leonardosarra@outlook.com>
2020-02-02 12:08:01 +0100

    Add support for pan and zoom in map

--- js/Map.js
+++ js/Map.js
@@ -10,7 +10,7 @@ var svg_map = d3.select(".map_area")
     .attr("transform", "translate(" + margin_map.left + "," + margin_map.top + ")");
 
 var projection = d3.geoMercator()
-    .center([10, 42])
+    .center([10, 52])
     .scale(400)
     .translate([width_map / 2, height_map / 2])
 
@@ -20,7 +20,13 @@ orchestrator.addListener('dataReady', function (e) {
     data = orchestrator.data;
     function evalData() {
         return orchestrator.getDataFilteredByParallel();
-      }
+    }
+
+    var zoom = d3.zoom()
+        .scaleExtent([1, 11])
+        .on('zoom', zoomed);
+
+    var last_zoom_transform;
     d3.json("./data/map.json", function (data) {
         //Drawing our map
         svg_map.append("g")
@@ -29,6 +35,7 @@ orchestrator.addListener('dataReady', function (e) {
             .enter()
             .append("path")
             .attr("fill", "#6fb1f3")
+            .attr("class", "map_path")
             .attr("d", d3.geoPath().projection(projection))
             .style("stroke", "black")
             .style("opacity", .3);
@@ -45,6 +52,7 @@ orchestrator.addListener('dataReady', function (e) {
             .attr("stroke-width", 3)
             .attr("fill-opacity", .4);
 
+        svg_map.call(zoom);
         orchestrator.addListener('scatterplotBrushing', function (e) {
             svg_map.select(".circles_container").selectAll("circle").data(evalData()).transition().duration(130).attr('stroke', function (d) {
                 if (orchestrator.filteringByScatterplot(d)) return 'red'
@@ -53,21 +61,22 @@ orchestrator.addListener('dataReady', function (e) {
         });
 
         orchestrator.addListener('parallelBrushing', function (e) {
-           updatePointsEntries();
+            updatePointsEntries();
         });
 
         orchestrator.addListener('updatedDataFiltering', function (e) {
             updatePointsEntries();
-         });
+        });
 
-        function updatePointsEntries(){
+        function updatePointsEntries() {
             modifiedData = evalData();
             var u = svg_map.select(".circles_container").selectAll("circle").data(modifiedData);
             u.exit().remove();
             u.enter().append('circle')
                 .attr("cx", function (d) { return projection([+d["longitude"], +d["latitude"]])[0]; })
                 .attr("cy", function (d) { return projection([+d["longitude"], +d["latitude"]])[1]; })
-                .attr("r", 0)
+                .attr('transform', last_zoom_transform)
+                .attr("r", 0);
 
             svg_map.select(".circles_container").selectAll("circle").data(modifiedData).transition().duration(200)
                 .attr("r", 1)
@@ -82,5 +91,11 @@ orchestrator.addListener('dataReady', function (e) {
                 .attr("stroke-width", 3)
                 .attr("fill-opacity", .4);
         }
-    })
+    });
+    function zoomed() {
+        last_zoom_transform = d3.event.transform;
+        svg_map.selectAll('.map_path') // To prevent stroke width from scaling
+            .attr('transform', last_zoom_transform);
+        svg_map.select(".circles_container").selectAll("circle").attr('transform', last_zoom_transform);
+    }
 });
