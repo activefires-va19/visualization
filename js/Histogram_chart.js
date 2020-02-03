6e643177ce6080af4c29134dd505f0b01e1eefe5
AlexS1995 <sarpe.1692247@studenti.uniroma1.it>
2020-02-03 23:23:05 +0100

    Fix hour axis histogram

--- js/Histogram_chart.js
+++ js/Histogram_chart.js
@@ -18,10 +18,13 @@ var selected_h = 1;
 
 function parse_hour_h(e) {
   var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
-  return new Date(mm + '/' + dd + '/' + yyyy+" " + e.substring(0, 2) + ":" + e.substring(2, 4));
+  
+  hh = e.substring(0, 2);
+  mm = e.substring(2, 4);
+  today.setHours(hh);
+  today.setMinutes(mm);
+
+  return today;
 }
 
 orchestrator.addListener('dataReady', function (e) {
