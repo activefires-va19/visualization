968e01b1da143d2b308cb99f703f84cc08e90a1f
AlexS1995 <sarpe.1692247@studenti.uniroma1.it>
2020-02-07 12:10:03 +0100

    Minor fix

--- js/Histogram_chart.js
+++ js/Histogram_chart.js
@@ -48,7 +48,7 @@ orchestrator.addListener('dataReady', function (e) {
   svg_h.append("g")
     .attr("class", "x-axis")
     .attr("transform", "translate(0," + height_h + ")")
-    .style("font-size", "8px")
+    .style("font-size", "9px")
     .call(xAxis);
 
   var histogram = d3.histogram()
